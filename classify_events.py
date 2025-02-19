8d773003065a09d695cfa51f277995dad547c615
Diego <lin61302@gmail.com>
2025-02-18 20:54:36 -0500

    cliff geoparsing update

--- classify_events.py
+++ classify_events.py
@@ -72,7 +72,7 @@ class EventClassifier:
         """
         colname = f'articles-{date.year}-{date.month}'
         print("Colname", colname)
-        source_domains = self.db.sources.distinct('source_domain', filter={'include' : True, 'primary_location' : {'$in' : ['CMR','TUN','LKA']}})
+        source_domains = self.db.sources.distinct('source_domain', filter={'include' : True, 'primary_location' : {'$in' : ['PAN']}})
         # source_domains = ['divergentes.com', 'revistafactum.com', 'alharaca.sv']
         # source_domains = self.db.sources.distinct('source_domain', filter={'include' : True, 'major_international' : True})
         # source_domains += self.db.sources.distinct('source_domain', filter={'include' : True, 'major_regional' : True})
