4b5e567f004eea442867e4fad8010a00ad1bed1b
Diego <lin61302@gmail.com>
2025-02-24 17:13:03 -0500

    civic/rai classfier update

--- location_cliff.py
+++ location_cliff.py
@@ -509,7 +509,7 @@ if __name__ == '__main__':
     ############
     int_reg = True
     AGO_ind = False
-    country_list = ['ENV_PAN','ENV_NIC','ENV_GTM']
+    country_list = ['PAN']
     source_domains = db.sources.distinct('source_domain', filter={'include' : True, 'primary_location' : {'$in' : country_list}})
     
     # source_domains = ['divergentes.com', 'revistafactum.com', 'alharaca.sv']
