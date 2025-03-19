fcf23121014c69ec64348db18aeb8bf3aeb940cc
Diego <lin61302@gmail.com>
2025-03-18 20:58:11 -0400

    Updated ModernBERT classifier for binary environmental classification

--- env_binary.py
+++ env_binary.py
@@ -12,8 +12,6 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 from helpers import build_combined
 import subprocess
-
-# NEW: Import tqdm for progress bars
 from tqdm import tqdm
 
 _ROOT = Path(os.path.abspath(os.path.dirname(__file__))).as_posix()
@@ -53,34 +51,24 @@ class EventClassifier:
 
     def load_model(self):
         """
-        Load the tokenizer from HF and then load local fine-tuned ModernBERT weights.
-        Bypasses any local tokenizer.json by using trust_remote_code with base_model_id.
+        Load the tokenizer and model from the specified directory.
         """
-        model_path = Path(self.model_location) / self.model_name
+        model_path = Path(self.model_location) / "environmental_binary"
         model_path_str = model_path.as_posix()
-        print(f"Loading local fine-tuned ModernBERT from: {model_path_str}")
+        print(f"Loading model from: {model_path_str}")
 
         base_model_id = "answerdotai/ModernBERT-large"
-        print(f"Loading tokenizer from: {base_model_id}")
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            base_model_id,
-            trust_remote_code=True
-        )
-
-        self.model = AutoModelForSequenceClassification.from_pretrained(
-            model_path_str,
-            trust_remote_code=True
-        )
+        self.tokenizer = AutoTokenizer.from_pretrained(base_model_id, trust_remote_code=True)
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_path_str, trust_remote_code=True)
         self.model.to(self.device)
         self.model.eval()
 
     def generate_cursor(self, date):
         """
-        Create a Mongo cursor for articles in the monthly collection 
-        that do NOT have self.model_name set and meet certain conditions.
+        Create a Mongo cursor for articles in the monthly collection that do NOT have self.model_name set.
         """
         colname = f'articles-{date.year}-{date.month}'
-        print(f"Colname: {colname}")
+        print(f"Processing collection: {colname}")
 
         source_domains = self.db.sources.distinct(
             'source_domain',
@@ -88,9 +76,10 @@ class EventClassifier:
                 'include': True,
                 'primary_location': {
                     '$in': [
-                        # 'CRI','PAK','HND','NIC','SLV','GTM','PAN'
-                        'SLB', 'NGA',
-                    ]
+                        'ENV_SLB','ENV_NIC','ENV_NGA','ENV_SLV','ENV_GTM','ENV_PAN',
+                        # 'SLB', 'NGA', 'CRI','PAK','HND','NIC','SLV','GTM','PAN'
+
+                        ]
                 }
             }
         )
@@ -98,36 +87,16 @@ class EventClassifier:
             { 
                 self.model_name: {'$exists': False},
                 'language_translated': 'en',
-                'title_translated': {
-                    '$exists': True, 
-                    '$ne': '', 
-                    '$ne': None, 
-                    '$type': 'string'
-                },
-                'maintext_translated': {
-                    '$exists': True, 
-                    '$ne': '', 
-                    '$ne': None, 
-                    '$type': 'string'
-                },
+                'title_translated': {'$exists': True, '$ne': '', '$ne': None, '$type': 'string'},
+                'maintext_translated': {'$exists': True, '$ne': '', '$ne': None, '$type': 'string'},
                 'source_domain': {'$in': source_domains},
-                'date_publish': {'$exists': True, '$ne': '', '$ne': None},
-                'environmental_binary.result': 'Yes'
+                'date_publish': {'$exists': True, '$ne': '', '$ne': None}
             }
         )
 
-    def check_index(self):
-        """
-        Ensure an index on self.model_name.
-        """
-        indexes = [list(idx['key'].keys())[0] for idx in self.db.articles.list_indexes()]
-        if self.model_name not in indexes:
-            self.db.articles.create_index([(self.model_name, 1)], background=True)
-
     def classify_articles(self):
         """
-        Given docs in self.queue, run them through the model, produce top1/top2 labels (if prob>0.3),
-        and store everything in self.top1_labels, self.top2_labels, self.all_model_outputs.
+        Run classification and store only the top predicted label.
         """
         texts = [build_combined(doc) for doc in self.queue]
 
@@ -143,36 +112,20 @@ class EventClassifier:
             logits = outputs.logits
             probs = torch.softmax(logits, dim=-1).cpu().numpy()
 
-        self.top1_labels = []
-        self.top2_labels = []
+        self.top_labels = []
         self.all_model_outputs = []
 
         for row in probs:
-            sorted_indices = np.argsort(row)  # ascending order
-            top1_idx = sorted_indices[-1]
-            top2_idx = sorted_indices[-2]
+            top_idx = np.argmax(row)
+            top_label = self.label_dict[top_idx]
 
-            top1_prob = float(row[top1_idx])
-            top2_prob = float(row[top2_idx])
-
-            top1_label = self.label_dict[top1_idx]
-            if top2_prob > 0.3:
-                top2_label = self.label_dict[top2_idx]
-            else:
-                top2_label = None
-
-            # Full distribution
-            label_scores = {
-                self.label_dict[i]: float(row[i]) for i in range(len(row))
-            }
-
-            self.top1_labels.append(top1_label)
-            self.top2_labels.append(top2_label)
+            label_scores = {self.label_dict[i]: float(row[i]) for i in range(len(row))}
+            self.top_labels.append(top_label)
             self.all_model_outputs.append(label_scores)
 
     def insert_info(self):
         """
-        Update the DB with env_max, env_sec, model_outputs for each doc in self.queue.
+        Update the DB with environmental_binary result and model outputs.
         """
         for nn, doc in enumerate(self.queue):
             try:
@@ -190,9 +143,8 @@ class EventClassifier:
                     {'_id': doc['_id']},
                     {
                         '$set': {
-                            f'{self.model_name}': {
-                                'env_max': self.top1_labels[nn],
-                                'env_sec': self.top2_labels[nn],
+                            'environmental_binary': {
+                                'result': self.top_labels[nn],
                                 'model_outputs': self.all_model_outputs[nn]
                             }
                         }
@@ -203,13 +155,11 @@ class EventClassifier:
 
     def clear_queue(self):
         self.queue = []
-        self.top1_labels = []
-        self.top2_labels = []
+        self.top_labels = []
         self.all_model_outputs = []
 
     def run(self):
         self.get_db_info()
-        self.check_index()
         self.load_model()
 
         dates = pd.date_range(
@@ -218,8 +168,7 @@ class EventClassifier:
             freq='M'
         )
 
-        # Outer progress bar: each month
-        for date in tqdm(dates, desc="Monthly Collections"):
+        for date in tqdm(dates, desc="Processing Monthly Collections"):
             try:
                 self.generate_cursor(date)
                 data_month = list(self.cursor)
@@ -231,16 +180,14 @@ class EventClassifier:
 
                 self.queue = []
 
-                # Inner progress bar: docs in this month
                 for doc in tqdm(data_month, desc=f"Classifying docs in {date}", leave=False):
                     self.queue.append(doc)
-                    # Classify in chunks of batch_size*10
+
                     if len(self.queue) >= (self.batch_size * 10):
                         self.classify_articles()
                         self.insert_info()
                         self.clear_queue()
 
-                # leftover
                 if len(self.queue) > 0:
                     self.classify_articles()
                     self.insert_info()
@@ -253,10 +200,10 @@ class EventClassifier:
 if __name__ == "__main__":
     classify_pipe(
         'mongodb://zungru:balsas.rial.tanoaks.schmoe.coffing@db-wibbels.sas.upenn.edu/?authSource=ml4p&tls=true',
-        'env_classifier',
+        'env_binary',
         '/home/diego/peace/modernbert_models',
         128
     )
 
-    commit_message = "ModernBERT classifier deployment update"
+    commit_message = "Updated ModernBERT classifier for binary environmental classification"
     run_git_commands(commit_message)
