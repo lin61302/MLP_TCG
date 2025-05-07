38db1f57ef8a0da97b538e8125060754586cea59
Diego <lin61302@gmail.com>
2025-05-07 13:42:10 -0400

    ModernBERT classifier deployment update

--- gemini_locations.py
+++ gemini_locations.py
@@ -360,7 +360,7 @@ if __name__ == "__main__":
         parser = GeminiBatchGeoParser(
             uri="mongodb://zungru:balsas.rial.tanoaks.schmoe.coffing@db-wibbels.sas.upenn.edu/?authSource=ml4p&tls=true",
             db_name="ml4p",
-            countries=[ 'COL','TUR','PER','UZB'] ,
+            countries=[ 'ENV_CMR','ENV_BGD','ENV_UZB','ENV_HUN','ENV_GEO','ENV_GHA','ENV_JAM','ENV_MWI','ENV_MRT','ENV_NER'] ,
                 
             #done: "ENV_GTM", "ENV_NGA", 'ENV_SLV', 'ENV_PAN', 'ENV_INT', 'ENV_CRI', 'ENV_SLB','ENV_NIC','ENV_BEN','ENV_PAK','ENV_HND' 
             # 'SLB', 'NGA', 'HND','NIC','SLV','GTM','PAN','CRI', 'CMR','TUN','LKA','UGA','NPL', 
@@ -374,6 +374,9 @@ if __name__ == "__main__":
             #'IDN','PAN','MKD','KGZ','MDA','SEN','SRB','LBR','NAM'
 
             # 'ENV_AZE','ENV_KGZ','ENV_IDN','ENV_MDA','ENV_MKD','ENV_COD','ENV_KAZ','ENV_LKA','ENV_ECU','ENV_GTM'
+            # processing 1: 'COL','TUR','PER','UZB'
+            # processing 2: 'ENV_CMR','ENV_BGD','ENV_UZB','ENV_HUN','ENV_GEO','ENV_GHA','ENV_JAM','ENV_MWI','ENV_MRT','ENV_NER'
+            
 
             start_year=2012,
             end_year=2025,
