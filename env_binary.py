b4c84f0da5530431c62b962bea25d2416d0ef662
Diego <lin61302@gmail.com>
2025-05-20 20:55:01 -0400

    Updated ModernBERT classifier for binary environmental classification

--- env_binary.py
+++ env_binary.py
@@ -107,6 +107,7 @@ class EventClassifier:
                         # 'ENV_COL','ENV_TUN','ENV_BLR','ENV_PRY','ENV_UGA','ENV_ETH','ENV_IDN','ENV_KHM','ENV_LBR','ENV_COD','ENV_MLI','ENV_NPL'
                         # 'UKR','DOM','ZMB','KEN'
                         # 'ENV_ECU'
+                        'UKR', 'DOM', 'ZMB', 'KEN',
                         'ENV_BLR'
 
                         
