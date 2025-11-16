2d1cc5b5effbf3186f90f02b7053d69c6490f6ab
Diego <lin61302@gmail.com>
2025-11-16 02:30:48 -0500

    Update ModernBERT inference for US_funding_* (writes to field named after model)

--- gemini_locations.py
+++ gemini_locations.py
@@ -362,10 +362,11 @@ if __name__ == "__main__":
             db_name="ml4p",
             countries=[
                 # 'MEX' 
-                 'MEX','LBR','MDA','SRB','LKA','KGZ','PHL'
+                #  'MEX','LBR','MDA','SRB','LKA','KGZ','PHL'
                 # 'MLI','ARM','SLV','ZMB','UGA'
                 # 'MOZ','COD','SSD','ZWE','GHA','KHM'
                 # 'BEN', 'UKR', 'GEO', 'GTM','NIC', 'PRY'
+                'UZB','DOM','BLR','AGO','XKX','ALB','MKD','BFA','CMR'
                     ] ,
                 
             #done: "ENV_GTM", "ENV_NGA", 'ENV_SLV', 'ENV_PAN', 'ENV_INT', 'ENV_CRI', 'ENV_SLB','ENV_NIC','ENV_BEN','ENV_PAK','ENV_HND' 
